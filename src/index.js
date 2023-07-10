84bd543d5466ca9fbe055bda8db6f94853336bdd
PhongDang <phongdang2899@gmail.com>
2023-07-10 20:08:09 +0700

    downgrade react 17

--- src/index.js
+++ src/index.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { ReactQueryDevtools } from '@tannerlinsley/react-query-devtools'
-import { QueryClient, QueryClientProvider } from 'react-query'
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import { ReactQueryDevtools } from '@tannerlinsley/react-query-devtools'
 
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+//     <BrowserRouter>
+//      <QueryClientProvider client={QueryClient}>
+//       <App />
+//       <ReactQueryDevtools initialIsOpen={false} />
+//       </QueryClientProvider>
+//     </BrowserRouter>
 
+// );
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import "./index.css";
+import App from "./App";
+
+const queryClient = new QueryClient();
+
+ReactDOM.render(
+  <React.StrictMode>
     <BrowserRouter>
-     <QueryClientProvider client={QueryClient}>
-      <App />
-      <ReactQueryDevtools initialIsOpen={false} />
+      <QueryClientProvider client={queryClient}>
+        <App />
       </QueryClientProvider>
     </BrowserRouter>
-  
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
