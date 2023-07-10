8f919248bc8e21bba71be40a335a5127b8b1536f
vietphop21 <anhphop1@gmail.com>
2023-07-10 19:29:57 +0700

    update

--- src/ListArticles.js
+++ src/ListArticles.js
@@ -6,106 +6,107 @@ import ReactPaginate from 'react-paginate'
 import CreateArticles from './CreateArticles'
 import EditArticles from './EditArticles'
 import { Button } from 'antd'
-import {Input} from 'antd'
+import { Input } from 'antd'
 import { ToastContainer, toast } from 'react-toastify'
 const ListArticles = () => {
-    const [render,setRender]=useState(false)
-    const [page,setPage]=useState(1)
+    const [render, setRender] = useState(false)
+    const [page, setPage] = useState(1)
     const [articles, setArticles] = useState([])
-    const [search,setSearch]=useState('')
+    const [search, setSearch] = useState('')
+   
 
     useEffect(() => {
         async function fetchData() {
-            try{
+            try {
                 let res = await axios.get(`https://617b71c2d842cf001711bed9.mockapi.io/api/v1/blogs?page=${page}&limit=10`)
-       
+
                 let data = res.data
-                console.log("Check data",data)
+                console.log("Check data", data)
                 setArticles(data)
                 window.scrollTo(0, 0);
 
-            } catch(error){
-            console.error(error);
+            } catch (error) {
+                console.error(error);
             }
 
         }
         fetchData()
-    }, [page,render]);
+    }, [page, render]);
 
-    const handlePageClick =(e)=>
-    {
+    const handlePageClick = (e) => {
         const s = e.selected + 1
         setPage(s)
 
     }
-    const handlesearch = async ()=>
-    {
+    const handlesearch = async () => {
         let res = await axios.get(`https://617b71c2d842cf001711bed9.mockapi.io/api/v1/blogs?search=${search}`)
+        localStorage.setItem('data', JSON.stringify(res.data))
+        sessionStorage.setItem('datasession', JSON.stringify(res.data))
+
         setArticles(res.data)
     }
-    const handleonChangesearch =(e)=>
-    {
-        
+    const handleonChangesearch = (e) => {
+
         setSearch(e.target.value)
         setRender(!render)
     }
-    const handleSort =async ()=>
-    {
-        let res=await axios.get (`https://617b71c2d842cf001711bed9.mockapi.io/api/v1/blogs?sortBy=id&order=desc`)
+    const handleSort = async () => {
+        let res = await axios.get(`https://617b71c2d842cf001711bed9.mockapi.io/api/v1/blogs?sortBy=id&order=desc`)
         setArticles(res.data)
     }
-    const handleDelete =async (id)=>{
+    const handleDelete = async (id) => {
         let res = await axios.delete(`https://617b71c2d842cf001711bed9.mockapi.io/api/v1/blogs/${id}`)
         toast.success("Delete success!")
         setRender(!render)
     }
     return (
         <>
-        
+
             <div class="titleproject">List Articles</div>
-            <ToastContainer/>
-            
-            <CreateArticles/>
-            <br/>
-            <Button style={{marginTop:'20px'}} onClick={handleSort} type="primary">Sort theo ID</Button>
-            <br/>
-           
-            <Input style={{width:'170px',marginTop:'20px'}}value={search} onChange={(e)=>handleonChangesearch(e)} placeholder="Tìm kiếm theo Title,Content" /> <Button onClick={handlesearch} type="primary">Search</Button>
+            <ToastContainer />
+
+
+            <CreateArticles />
+            <br />
+            <Button style={{ marginTop: '20px' }} onClick={handleSort} type="primary">Sort theo ID</Button>
+            <br />
+
+            <Input style={{ width: '170px', marginTop: '20px' }} value={search} onChange={(e) => handleonChangesearch(e)} placeholder="Tìm kiếm theo Title,Content" /> <Button onClick={handlesearch} type="primary">Search</Button>
 
             <div>
-            {articles.map((item) => {
-                return (
+                {articles.map((item) => {
+                    return (
 
-                    <>
-                        <div class="container-articles">
-                            <div style={{width:'150px'}}>
-                            <img src={item.image}></img>
-                            </div>
-                            <div>
-                                <p> ID: {item.id}</p>
-                                <p class="title">Title: {item.title} </p>
-                                <p class="content">Content: {item.content}</p>
-                                <EditArticles
-                                render={render}
-                                setRender={setRender}
-                               
-                                item={item}
-                                />
-                                <DetailsArticles
-                                id={item.id}
-                                />
-                                <button class="btndelete" type="button" onClick={()=>handleDelete(item.id)}>Delete</button>
-                                
-                                
-                            </div>
-                        </div>
+                        <>
+                            <div class="container-articles">
+                                <div style={{ width: '150px' }}>
+                                    <img src={item.image}></img>
+                                </div>
+                                <div>
+                                    <p> ID: {item.id}</p>
+                                    <p class="title">Title: {item.title} </p>
+                                    <p class="content">Content: {item.content}</p>
+                                    <EditArticles
+                                        render={render}
+                                        setRender={setRender}
 
-                    </>
-                )
-            })}
+                                        item={item}
+                                    />
+                                    <DetailsArticles
+                                        id={item.id}
+                                    />
+                                    <button class="btndelete" type="button" onClick={() => handleDelete(item.id)}>Delete</button>
+
+
+                                </div>
+                            </div>
+
+                        </>
+                    )
+                })}
             </div>
-           <div class="page">
-           <ReactPaginate
+            <div class="page">
+                <ReactPaginate
                     nextLabel="Next  >"
                     onPageChange={handlePageClick}
                     pageRangeDisplayed={5}
@@ -125,7 +126,7 @@ const ListArticles = () => {
                     activeClassName="active"
                     renderOnZeroPageCount={null}
                 />
-           </div>
+            </div>
         </>
     )
 }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { ReactQueryDevtools } from '@tannerlinsley/react-query-devtools'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+
+    <BrowserRouter>
+     <QueryClientProvider client={QueryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </BrowserRouter>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
